099e0a1c28817b2ea94c95445009387bd802204c
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-07-24 20:33:53 +0300

    chore: add feat 3 - string teplate literals

--- js/feat-1-const-let.js
+++ js/feat-1-const-let.js
@@ -1,10 +1,8 @@
 // 1. const, let keywords
 
-const name 
+const name = "Mweero";
 
-
-console.log(name, "lin3")
+console.log(name, "lin3");
 //
- name ="Mwero"
-console.log(name," lin6")
-console.log(`Hello ${name}`)
+console.log(name, " lin6");
+console.log(`Hello ${name}`);
---
+++ js/feat-2-arrow-funcs.js
@@ -0,0 +1,25 @@
+// 2/ array functions
+function add(a, b) {
+  let result = a + b;
+  return result;
+}
+
+const sum = (a, b) => a + b;
+
+const prod = (a, b) => {
+  let result = a * b;
+  return result;
+};
+
+function area (length){
+    return length **2
+}
+
+const aRea = length => length**2
+
+const greet = ()=>console.log("Hello World")
+
+
+
+console.log(sum(-4, 9));
+console.log(add(8, 9));
