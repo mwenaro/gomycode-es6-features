a8bbf00e68ffb0a76e9c92097944b19106929e80
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-07-25 19:48:52 +0300

    chore : upadte feat-5 file

--- js/feat-5-rest-operator.js
+++ js/feat-5-rest-operator.js
@@ -1,14 +1,30 @@
-// 5 . Rest operator
-const user = {
-    firstName: "Mwero",
-    lastName: "Abdalla",
-    age: 56,
-    address:{
-      box:19,
-      town:"Mazeras",
-      code:80114
-    }
-  };
+// Rest operations
+const student = {
+  name: "Mwero",
+  amd: 1570,
+  marks: {
+    eng: 80,
+    mat: 92,
+    swa: 72,
+    sci: 88,
+    ssre: 83,
+  },
+};
+// const { marks, ...rest } = student;
 
-  const {address,firstName, ...rest} = user
-  console.log(rest)
+// console.log({ marks });
+// console.log({ rest });
+
+// console.log({ student });
+
+const hobbies = [
+  "feeding",
+  "playing football",
+  "reading storybooks",
+  "programming",
+];
+
+const [first,second,third, ...others] = hobbies
+console.log(first)
+console.log(others)
+
--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-console.log("Hello there")
+// Rest operations
+const student = {
+  name: "Mwero",
+  amd: 1570,
+  marks: {
+    eng: 80,
+    mat: 92,
+    swa: 72,
+    sci: 88,
+    ssre: 83,
+  },
+};
+// const { marks, ...rest } = student;
+
+// console.log({ marks });
+// console.log({ rest });
+
+// console.log({ student });
+
+const hobbies = [
+  "feeding",
+  "playing football",
+  "reading storybooks",
+  "programming",
+];
+
+const [first,second,third, ...others] = hobbies
+console.log(first)
+console.log(others)
+
